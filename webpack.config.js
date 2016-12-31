d370e11d460d317dbebaf63bf332eb013d18645e
melurie <melurie03@gmail.com>
2016-12-31 15:40:52 -0600

    starting to build webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,20 @@
 'use strict';
 
-const webpack = require("webpack");
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {test: /\.(js|jsx)$/, use: 'babel-loader'}
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+  ]
+};
