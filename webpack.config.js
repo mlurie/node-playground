87f9f9f1bda5cae82737018a478537f2a6ad97e4
melurie <melurie03@gmail.com>
2016-12-31 18:16:01 -0600

    Building web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /\.(js|jsx)$/, use: 'babel-loader'}
+      {test: /\.(js|jsx)$/, use: ['babel-loader','eslint-loader']}
     ]
   },
   plugins: [
